3b70d45d21fd2e69d096045ed8fbae68bccec5fe
WiltonChrysostomo <36368451+WiltonChrysostomo@users.noreply.github.com>
2018-03-02 09:39:31 -0300

    Add files via upload

---
+++ PrimeiraAulaReverdan/PrimeiraAulaReverdan/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace PrimeiraAulaReverdan
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.texPrimeiroNumero = new System.Windows.Forms.TextBox();
+            this.texSegundoNumero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(23, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o 1° número";
+            // 
+            // texPrimeiroNumero
+            // 
+            this.texPrimeiroNumero.Location = new System.Drawing.Point(26, 34);
+            this.texPrimeiroNumero.Name = "texPrimeiroNumero";
+            this.texPrimeiroNumero.Size = new System.Drawing.Size(176, 20);
+            this.texPrimeiroNumero.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.texPrimeiroNumero, "Digite um numero");
+            // 
+            // texSegundoNumero
+            // 
+            this.texSegundoNumero.Location = new System.Drawing.Point(26, 98);
+            this.texSegundoNumero.Name = "texSegundoNumero";
+            this.texSegundoNumero.Size = new System.Drawing.Size(176, 20);
+            this.texSegundoNumero.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.texSegundoNumero, "Wilton");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(23, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite o 2° número";
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnSomar.Location = new System.Drawing.Point(26, 145);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 4;
+            this.btnSomar.Text = "&Somar +";
+            this.toolTip1.SetToolTip(this.btnSomar, "Botao para resultado");
+            this.btnSomar.UseVisualStyleBackColor = false;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.lblResultado.ForeColor = System.Drawing.Color.Red;
+            this.lblResultado.Location = new System.Drawing.Point(23, 221);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.BackColor = System.Drawing.Color.Red;
+            this.btnSubtrair.Location = new System.Drawing.Point(127, 145);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtrair.TabIndex = 6;
+            this.btnSubtrair.Text = "Subtrair -";
+            this.btnSubtrair.UseVisualStyleBackColor = false;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.Yellow;
+            this.btnDividir.ForeColor = System.Drawing.Color.Black;
+            this.btnDividir.Location = new System.Drawing.Point(127, 183);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(75, 23);
+            this.btnDividir.TabIndex = 7;
+            this.btnDividir.Text = "Dividir /";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnMultiplicar.Location = new System.Drawing.Point(26, 183);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiplicar.TabIndex = 8;
+            this.btnMultiplicar.Text = "Multiplicar *";
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(228, 261);
+            this.Controls.Add(this.btnMultiplicar);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.texSegundoNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.texPrimeiroNumero);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "frmPrincipal";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox texPrimeiroNumero;
+        private System.Windows.Forms.TextBox texSegundoNumero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+    }
+}
+
---
+++ PrimeiraAulaReverdan/PrimeiraAulaReverdan/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiraAulaReverdan
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+        void Calcular(string OP)
+        {
+            Double n1 = 0.0, n2 = 0.0, resultado = 0.0;
+            try
+            {
+                n1 = Convert.ToDouble(texPrimeiroNumero.Text);
+                n2 = Convert.ToDouble(texSegundoNumero.Text);
+
+                if (OP.Equals("+"))
+                    resultado = n1 + n2;
+
+                if (OP.Equals("-"))
+                    resultado = n1 - n2;
+
+                if (OP.Equals("*"))
+                    resultado = n1 * n2;
+
+                if (OP.Equals("/"))
+                    resultado = n1 / n2;
+                lblResultado.Text = resultado.ToString();
+            }
+
+            catch (FormatException e)
+            {
+                MessageBox.Show(" Escreva um número correto");
+            }
+
+        }
+            
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            Calcular("+");
+       
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            Calcular("-");
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            Calcular("*");
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            Calcular("/");
+        }
+    }
+}
